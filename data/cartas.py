91f0713a7a6985b5ada8d0ec62b59a62e8caae4b
rubences <91731294+rubences@users.noreply.github.com>
2022-11-25 09:30:23 +0000

    definir cartas

---
+++ data/cartas.py
@@ -0,0 +1,16 @@
+def cartas()
+    cartas = { 
+        chr(0x1f0a1): 11, 
+        chr(0x1f0a2): 2, 
+        chr(0x1f0a3): 3, 
+        chr(0x1f0a4): 4, 
+        chr(0x1f0a5): 5, 
+        chr(0x1f0a6): 6, 
+        chr(0x1f0a7): 7, 
+        chr(0x1f0a8): 8, 
+        chr(0x1f0a9): 9, 
+        chr(0x1f0aa): 10, 
+        chr(0x1f0ab): 10, 
+        chr(0x1f0ad): 10, 
+        chr(0x1f0ae): 10, 
+    } 
